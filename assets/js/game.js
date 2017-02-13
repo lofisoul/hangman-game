a176a9ddc3f7702fe1564c73e3c4b1fa2714606b
lofisoul <jason.nolfi@gmail.com>
2017-02-12 20:14:51 -0600

    Attempt 2. Restructured the script and now the framework of the logic is in place.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,56 +1,6 @@
-// Press any key to get started!
-//
-// Wins: (# of times user guessed the word correctly).
-//
-// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-//
-// As the user guesses the correct letters, reveal them: m a d o _ _ a.
-//
-// Number of Guesses Remaining: (# of guesses remaining for the user).
-//
-// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
-//
-// After the user wins/loses the game should automatically choose another word and make the user play it.
-//
-// Hangman Game Bonuses
-//
-// Play a sound or song when the user guesses their word correctly, like in our demo.
-// Write some stylish CSS rules to make a design that fits your game's theme.
-// HARD MODE: Organize your game code as an object, except for the key events to get the letter guessed. This will be a challenge if you haven't coded with JavaScript before, but we encourage anyone already familiar with the language to try this out.
-// Save your whole game and its properties in an object.
-// Save any of your game's functions as methods, and call them underneath your object declaration using event listeners.
-// Don't forget to place your global variables and functions above your object.
-// Remember: global variables, then objects, then calls.
+//GLOBAL vars
 
-/*
-LOGIC:
-
-press any key to start the game (start with an interstitial and fade out when clicked) initGame = true
-grab a players name from JSON object player.FirstName + player.LastName arr[randomnum] // ajax request
-save that to var -> playerGuess
-
-function guess(e.key) {
-  if press letter from var {
-  success add that to the screen and add a class that opacities the letter (visual)
-}
-
-else {
-opacity the letter
-mark an incorrect guess --> 6 incorrect guesses (head, torso, left arm, right arm, left leg, right leg)
-}
-
-disableKey = true // this should say that if you press this key again ... nothing will happen
-
-}
-
-win -> you win pop up
-lose -> you lose pop up
-
-score (wins and losses) -> displayed on a scoreboard or a div like a scoreboard [http://www.dafont.com/score-board.font]
-
-
-*/
-
+//list of players to choose from
 var players = [
   {
     'firstName' : 'Dion',
@@ -71,88 +21,70 @@ var players = [
   {
     'firstName' : 'CJ',
     'lastName' : 'McCollum'
+  },
+  {
+    'firstName' : 'Kyrie',
+    'lastName' : 'Irving'
+  },
+  {
+    'firstName' : 'John',
+    'lastName' : 'Wall'
+  },
+  {
+    'firstName' : 'Rudy',
+    'lastName' : 'Gobert'
   }
 ];
 
-var lettersToGuess;
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var answer;
-var lettersGuessedRight = [];
-var lettersGuessedWrong = [];
-var game = {
-  wins: 0,
-  losses: 0,
-  guesses: 0,
-  maxGuesses: 6,
-  isStarted: false,
-  initGame: function(e) {
-      //set vars for game start
-      this.wins = 0;
-      this.losses = 0;
-      this.guesses = 0;
-      this.maxGuesses = 6;
-      game.isStarted = true;
-      $('#wins').html(game.wins);
-      $('#losses').html(game.losses);
-      $('#guesses').html(game.maxGuesses);
-      var randomPlayer = players[Math.floor(Math.random()*players.length)];
-      var playerLetters = randomPlayer.firstName + randomPlayer.lastName;
-      lettersToGuess = playerLetters.toLowerCase();
-      console.log(randomPlayer.firstName + ' ' + randomPlayer.lastName);
-      for (i=0; i<randomPlayer.firstName.length; i++) {
-        $('.first-name').append('<div class="guess-box" data-letter="'+ randomPlayer.firstName.charAt(i).toLowerCase()+'"></div>');
-      }
-      for (i=0; i<randomPlayer.lastName.length; i++) {
-        $('.last-name').append('<div class="guess-box" data-letter="'+ randomPlayer.lastName.charAt(i).toLowerCase()+'"></div>');
-      }
-      var playerArr = stringThatArr(lettersToGuess);
-      answer = playerArr.filter(function(elem, index, self) {
-          return index == self.indexOf(elem);
-      });
-      var badLetters = alphabet.filter( function( el ) {
-        return playerArr.indexOf( el ) < 0;
-      } );
-  },
-  removeStage: function() {
-    $('.start-screen').addClass('game-on');
-  },
-  //pressLetter = []; --> push e.key if in here then ignore!
-  //pressAnyKey = []; push. first key press --> stop the array
-  guessLetter: function(e) {
-    console.log('This is a guess')
-  },
-  playGame: function(e) {
-    if(game.isStarted === false) {
-      game.initGame();
-      game.removeStage();
-    }
-    else {
-      game.guess(e);
-    }
-  },
-  guess: function(e) {
-    if (isInArray(answer,e.key)) {
-      //console.log('This Letter is in the answer');
-      var guessDiv = $('.guess-box[data-letter="'+e.key+'"]');
-      $(guessDiv).html('<span>' + e.key + '</span>');
-      lettersGuessedRight.push(e.key);
-      }
-      else {
-        //wrong guess
-        game.maxGuesses--;
-        $('#guesses').html(game.maxGuesses);``
-        if(game.maxGuesses === 0) {
-          alert('You Lose!')
-        }
-      }
-    },
-    winGame: function() {
-      if(lettersGuessedRight.length === answer.length) {
-        console.log('You Win');
-        $('.start-screen').removeClass('game-on');
-        game.initGame();
-      }
-    }
+var wins = 0; //wins start at 0
+var losses = 0; //losses start at 0
+var answer; //this is the player's first name and last name [not sure if this should be an array]
+var isStarted = false; // the game is off to start
+var guesses = 0 // this is a counter of the number of guesses that you make
+var wrongGuesses = 6 // when this hits 0 then the game is over and you lose
+var clickCount = 0 //maybe need this for game start
+var lettersGuessed = [];
+var correctGuesses = [];
+
+function startGame(e) {
+  if(isStarted === true) {
+    return;
+  }
+  else {
+    removeStage();
+    initGame();
+  }
+}
+
+function removeStage() {
+  $('.start-screen').addClass('game-on');
+}
+
+function initGame() {
+  $('#guesses').html(wrongGuesses);
+  $('#wins').html(wins);
+  $('#losses').html(losses);
+  generatePlayer();
+  isStarted = true;
+}
+
+function generatePlayer() {
+  var randomPlayer = players[Math.floor(Math.random()*players.length)];
+  var playerLetters = randomPlayer.firstName + randomPlayer.lastName;
+  var playerLetters = playerLetters.toLowerCase();
+  //create divs on page for random player
+  for (i=0; i<randomPlayer.firstName.length; i++) {
+    $('.first-name').append('<div class="guess-box" data-letter="'+ randomPlayer.firstName.charAt(i).toLowerCase()+'"></div>');
+  }
+  for (i=0; i<randomPlayer.lastName.length; i++) {
+    $('.last-name').append('<div class="guess-box" data-letter="'+ randomPlayer.lastName.charAt(i).toLowerCase()+'"></div>');
+  }
+  var nameArray = stringThatArr(playerLetters);
+  answer = nameArray.filter(function(elem, index, self) {
+      return index == self.indexOf(elem);
+  });
+  console.log(answer)
+  return answer;
 }
 
 function stringThatArr(name) {
@@ -160,8 +92,87 @@ function stringThatArr(name) {
   return chars;
 }
 
+
+function playGame(e) {
+  if (isStarted === false) {
+    return;
+  }
+
+  else {
+    guessLetter(e);
+  }
+}
+
+function guessLetter(e) {
+  markLetterAsGuessed(e, lettersGuessed);
+  if (isInArray(answer,e.key)) {
+    markLetterAsGuessed(e, correctGuesses);
+    console.log('This Letter is in the answer ' + correctGuesses);
+    var guessDiv = $('.guess-box[data-letter="'+e.key+'"]');
+    $(guessDiv).html('<span>' + e.key + '</span>');
+    winGame();
+    }
+    else {
+      //wrong guess
+      console.log('This is a wrong Guess!');
+      wrongGuesses--;
+      $('#guesses').html(wrongGuesses);
+      loseGame();
+    }
+  //this should take 2 conditions --> right guess and wrong guess
+  //right guess adds letters to guess boxes
+  //wrong guess subtracts guesses remaining
+    // if (isInArray(answer,e.key)) {
+    //   //console.log('This Letter is in the answer');
+    //   var guessDiv = $('.guess-box[data-letter="'+e.key+'"]');
+    //   $(guessDiv).html('<span>' + e.key + '</span>');
+    //   lettersGuessedRight.push(e.key);
+    // }
+    // else {
+    //   //wrong guess
+    //   game.maxGuesses--;
+    //   $('#guesses').html(game.maxGuesses);``
+    //
+    // }
+}
+
+function markLetterAsGuessed(e, arr) {
+  if(isInArray(arr,e.key)){
+    console.log('already guessed!');
+    return;
+  }
+  else {
+  console.log(e.key);
+  arr.push(e.key);
+  console.log(arr);
+ }
+}
+
 function isInArray(arr, item) {
   return arr.indexOf(item.toLowerCase()) > -1;
 }
 
-window.addEventListener('keydown', game.playGame);
+function winGame() {
+  if(answer.length === correctGuesses.length) {
+  alert('You win!');
+  wins++;
+  $('#wins').html(wins);
+  playAgain('winning');
+  }
+}
+
+function loseGame() {
+  if(wrongGuesses === 0) {
+    losses++;
+    $('#losses').html(losses);
+    playAgain('losing');
+    alert('you lose!');
+  }
+}
+
+function playAgain(className) {
+  $('.stage').removeClass('game-on').addClass(className);
+}
+
+$(window).on('keyup', startGame);
+$(window).on('keydown', playGame);
